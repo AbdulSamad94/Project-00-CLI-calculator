794be9b19527419de517fa1e29808ea6bcc2def0
Abdul Samad Siddiqui <160336057+AbdulSamad94@users.noreply.github.com>
2024-03-16 00:59:37 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+import inquirer from "inquirer";
+const Target = await inquirer.prompt([{
+        message: "Enter Your First Number",
+        type: "number",
+        name: "Num1",
+    },
+    {
+        message: "Enter An Operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "division", "Multiplication"],
+    },
+    {
+        message: "Enter Your Second Number",
+        type: "number",
+        name: "Num2",
+    }
+]);
+if (Target.operator === "Addition") {
+    console.log(Target.Num1 + Target.Num2);
+}
+else if (Target.operator === "Subtraction") {
+    console.log(Target.Num1 - Target.Num2);
+}
+else if (Target.operator === "division") {
+    console.log(Target.Num1 / Target.Num2);
+}
+else if (Target.operator === "Multiplication") {
+    console.log(Target.Num1 + Target.Num2);
+}
